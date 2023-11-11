b6e3a94530747606bc79cd490a998fc8dfb6f86c
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-11-11 11:01:32 +0000

    Fixed UI updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import ReactDOM from '../node_modules/@types/react-dom/client.ts';
+import App from './App.jsx';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BlockchainProvider } from './context/BlockchainContext.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BlockchainProvider>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BlockchainProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
